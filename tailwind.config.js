6eefe7ffdbe10314bdfbd13caf353324bd68a36b
Ricardo Alf <rear_recinos@hotmail.com>
2023-02-24 19:28:31 -0600

    fisrt commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			colors: {
+				// Primary
+				darkCyan: 'hsl(185, 75%, 39%)',
+				veryDarkDesaturatedBlue: 'hsl(229, 23%, 23%)',
+				darkGrayishBlue: 'hsl(227, 10%, 46%)',
+				// Neutral
+				darkGray: 'hsl(0, 0%, 59%)',
+			},
+			fontSize: {
+				// Body
+				fontSize_name_stats: '10px',
+			},
+		},
+	},
+}
